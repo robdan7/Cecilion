93f74939b31ebcaccda4eb14418df3e94d82cc03
robdan7 <robdan-7@student.ltu.se>
2020-03-13 14:08:28 +0100

    Added GLEW and a simple demo for testing purposes

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,7 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -16,14 +19,31 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    /* Make the window's context current. GLEW needs this before we can use it. */
     glfwMakeContextCurrent(window);
+
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;  // Print the OpenGL driver version
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f,-0.5f);
+        glVertex2f(0,0.5f);
+        glVertex2f(0.5f,-0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
